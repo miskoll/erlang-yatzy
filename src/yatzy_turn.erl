fb29a34d0569130111794dd13a9e54c7f844e838
Scott Jones <scott.jones@alertlogic.com>
2018-12-04 09:10:38 -0600

    Implement src/yatzy_turn.erl as a gen_statem

--- src/yatzy_turn.erl
+++ src/yatzy_turn.erl
@@ -1,115 +1,122 @@
 -module(yatzy_turn).
--export([start/0, roll/2, dice/1, stop/1, rolls_left/1]).
--spec start() -> {'ok', TurnPid::pid()}.
+-export([roll/1,
+         dice/0,
+         stop/0,
+         rolls_left/0]).
+-behavior(gen_statem).
+-export([start_link/0,
+         init/1,
+         callback_mode/0
+        ]).
 
--spec roll(TurnPid::pid(), Keep::[1..6]) -> 'ok' | 'invalid_keepers' 
-                                                 | 'finished'.
-%% Once the player has selected which dice to keep roll the remaining 
+%% States
+-export([second_roll/3,
+         third_roll/3,
+         end_state/3
+        ]).
+
+
+%% SPEC -------------------------------------------
+-spec start_link() -> {'ok', TurnPid::pid()}.
+
+-spec roll(Keep::[1..6]) -> 'ok' | 'invalid_keepers'
+                                 | 'finished'.
+%% Once the player has selected which dice to keep roll the remaining
 %% dice unless they have already been rolled twice.
 
--spec dice(TurnPid::pid()) -> yatzy:roll().
+-spec dice() -> yatzy:roll().
 %% Just the rolled dice as it stands at this point.
 
--spec stop(TurnPid::pid()) -> yatzy:roll().
+-spec stop() -> yatzy:roll().
 %% Just stop the procees and get out what was rolled.
 
--spec rolls_left(TurnPid::pid()) -> yatzy:roll().
+-spec rolls_left() -> yatzy:roll().
 %% Returns the number of rolls remaining in the turn `TurnPid'.
 
-start() ->
-  NumDice = 5,
-  TurnPid = spawn(fun() -> first_roll(randomize_dice(NumDice)) end),
-  {ok, TurnPid}.
+start_link() ->
+    %% start_link(name, module_containing_callback_functions, args, options)
+    gen_statem:start_link({local, ?MODULE}, ?MODULE, null, []).
 
-dice(Pid) ->
-  call(Pid, {self(), dice}).
+init(null) ->
+    NumDice = 5,
+    FirstRoll = randomize_dice(NumDice),
+    {ok, second_roll, FirstRoll}.
 
-stop(Pid) ->
-  call(Pid, {self(), stop}).
+callback_mode() ->
+    state_functions.
 
-roll(Pid, Keep) ->
-  call(Pid, {self(), {roll, Keep}}).
+dice() ->
+    gen_statem:call(?MODULE, dice).
 
-rolls_left(Pid) ->
-  call(Pid, {self(), rolls_left}).
+stop() ->
+    gen_statem:call(?MODULE, stop).
 
-first_roll(Roll) ->
-  RollsLeft = 2,
-  receive
-    {From, dice} ->
-      From ! Roll,
-      first_roll(Roll);
-    {From, stop} ->
-      From ! Roll;
-    {From, rolls_left} ->
-      From ! RollsLeft,
-      first_roll(Roll);
-    {From, {roll, Keep}} ->
-      case is_valid_keep(Keep, Roll) of
-        true -> 
-          NewRoll = randomize_dice(5 - length(Keep)) ++ Keep,
-          From ! {ok, NewRoll},
-          second_roll(NewRoll);
-        false -> 
-          From ! invalid_keepers,
-          first_roll(Roll)
-      end
-  end.
+roll(Keep) ->
+    gen_statem:call(?MODULE, {roll, Keep}).
 
-second_roll(Roll) ->
-  RollsLeft = 1,
-  receive
-    {From, dice} ->
-      From ! Roll,
-      second_roll(Roll);
-    {From, stop} ->
-      From ! Roll;
-    {From, rolls_left} ->
-      From ! RollsLeft,
-      second_roll(Roll);
-    {From, {roll, Keep}} ->
-      case is_valid_keep(Keep, Roll) of
+rolls_left() ->
+    gen_statem:call(?MODULE, rolls_left).
+
+%% FIRST ROLL =====================================
+%% dice
+first_roll({call, From}, dice, Roll) ->
+    {keep_state_and_data, [{reply, From, Roll}]};
+%% {roll, Keep}
+first_roll({call, From}, {roll, Keep}, Roll) ->
+    case is_valid_keep(Keep, Roll) of
         true ->
-          NewRoll = randomize_dice(5 - length(Keep)) ++ Keep,
-          From ! {ok, NewRoll},
-          third_roll(NewRoll);
+            NewRoll = randomize_dice(5 - length(Keep)) ++ Keep,
+            {next_state, second_roll, NewRoll, [{reply, From, NewRoll}]};
         false ->
-          From ! invalid_keepers,
-          second_roll(Roll)
-      end
-  end.
+            {keep_state_and_data, [{reply, From, invalid_keepers}]}
+    end;
+%% rolls_left
+first_roll({call, From}, rolls_left, _Roll) ->
+    RollsLeft = 2,
+    {keep_state_and_data, [{reply, From, RollsLeft}]};
+%% stop
+first_roll({call, From}, stop, Roll) ->
+    {stop_and_reply, normal, {reply, From, Roll}}.
 
-third_roll(Roll) ->
-  RollsLeft = 0,
-  receive
-    {From, dice} ->
-      From ! Roll,
-      third_roll(Roll);
-    {From, {roll, _Keep}} ->
-      From ! finished,
-      third_roll(Roll);
-    {From, rolls_left} ->
-      From ! RollsLeft,
-      third_roll(Roll);
-    {From, stop} ->
-      From ! Roll
-  end.
+%% SECOND ROLL =====================================
+%% dice
+second_roll({call, From}, dice, Roll) ->
+    {keep_state_and_data, [{reply, From, Roll}]};
+%% {roll, Keep}
+second_roll({call, From}, {roll, Keep}, Roll) ->
+    case is_valid_keep(Keep, Roll) of
+        true ->
+            NewRoll = randomize_dice(5 - length(Keep)) ++ Keep,
+            {next_state, third_roll, NewRoll, {reply, From, NewRoll}};
+        false ->
+            {keep_state_and_data, [{reply, From, invalid_keepers}]}
+    end;
+%% rolls_left
+second_roll({call, From}, rolls_left, _Roll) ->
+    RollsLeft = 1,
+    {keep_state_and_data, [{reply, From, RollsLeft}]};
+%% stop
+second_roll({call, From}, stop, Roll) ->
+    {stop_and_reply, normal, {reply, From, Roll}}.
+
+%% THIRD ROLL =======================================
+%% dice
+third_roll({call, From}, dice, Roll) ->
+    {keep_state_and_data, [{reply, From, Roll}]};
+%% {roll, Keep}
+third_roll({call, From}, {roll, _Keep}, _Roll) ->
+    {keep_state_and_data, [{reply, From, finished}]};
+%% rolls_left
+third_roll({call, From}, rolls_left, _Roll) ->
+    RollsLeft = 0,
+    {keep_state_and_data, [{reply, From, RollsLeft}]};
+%% stop
+third_roll({call, From}, stop, Roll) ->
+    {stop_and_reply, normal, {reply, From, Roll}}.
 
 is_valid_keep(Keep, Roll) ->
-  length(Keep -- Roll) == 0.
-
-call(Pid, Msg) ->
-  case is_process_alive(Pid) of
-    true ->
-      Pid ! Msg,
-      receive
-        Response ->
-          Response
-      end;
-    false ->
-      {error, dead_process}
-  end.
+    length(Keep -- Roll) == 0.
 
 randomize_dice(NumberOfDice) ->
-  NumPossibleValues = 6,
-  [rand:uniform(NumPossibleValues) || _ <- lists:seq(1,NumberOfDice)].
+    NumPossibleValues = 6,
+    [rand:uniform(NumPossibleValues) || _ <- lists:seq(1,NumberOfDice)].
